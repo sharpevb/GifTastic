741b682bda14885d57cc23723b27c56937487604
sharpevb <victoriabsharpe@gmail.com>
2019-06-23 19:20:26 -0400

    adding giphy api call

---
+++ assets/javascript/giphy.js
@@ -0,0 +1,26 @@
+$("button").on("click", function() {
+var animal = $(this).attr("data-animal");
+
+var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + animal + "&api_key=JRcxsQnkDertgsjaSuehdzHxlznU528v";
+
+$.ajax( {
+    url: queryURL,
+    method: "GET"
+})
+.then(function(response) {
+    console.log(queryURL);
+    console.log(response);
+    var results = response.data;
+
+    for (var i=0; i < results.length; i++) {
+        var animalDiv = $("<div>")
+        var p = $("<p>").text("Rating: " + results[i].rating);
+        var animalImg = $("<img>");
+        animalImg.attr("src", results[i].images.fixed_height_still.url);
+        animalDiv.append(p);
+        animalDiv.append(animalImg);
+        $("#gifs-here").prepend(animalDiv);
+    }
+})
+
+});
