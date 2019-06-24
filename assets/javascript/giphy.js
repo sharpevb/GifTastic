ae5493850b8fbac24fc15f7d35c84a5511b3d60d
sharpevb <victoriabsharpe@gmail.com>
2019-06-23 21:02:36 -0400

    trying to get gifs to animate on click

--- assets/javascript/giphy.js
+++ assets/javascript/giphy.js
@@ -1,26 +1,52 @@
-$("button").on("click", function() {
-var animal = $(this).attr("data-animal");
+$("button").on("click", function () {
+    var animal = $(this).attr("data-animal");
 
-var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + animal + "&api_key=JRcxsQnkDertgsjaSuehdzHxlznU528v&limit=10";
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + animal + "&api_key=JRcxsQnkDertgsjaSuehdzHxlznU528v&limit=10";
 
-$.ajax( {
-    url: queryURL,
-    method: "GET"
-})
-.then(function(response) {
-    console.log(queryURL);
-    console.log(response);
-    var results = response.data;
+    var state = $(this).attr("data-state");
 
-    for (var i=0; i < results.length; i++) {
-        var animalDiv = $("<div>")
-        var p = $("<p>").text("Rating: " + results[i].rating);
-        var animalImg = $("<img>");
-        animalImg.attr("src", results[i].images.fixed_height_still.url);
-        animalDiv.append(p);
-        animalDiv.append(animalImg);
-        $("#gifs-here").prepend(animalDiv);
-    }
-})
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    })
+        .then(function (response) {
+            console.log(queryURL);
+            console.log(response);
+            var results = response.data;
+
+            for (var i = 0; i < results.length; i++) {
+                    var animalDiv = $("<div>")
+                    var p = $("<p>").text("Rating: " + results[i].rating);
+                    var animalImg = $("<img>");
+                    animalImg.attr("src", results[i].images.fixed_height_still.url);
+                    animalDiv.append(p);
+                    animalDiv.append(animalImg);
+                    $("#gifs-here").prepend(animalDiv);
+                    //add data-state="animate" tag to img tag
+
+                animalImg.on("click", function () {
+                    console.log("click");
+                    var animalAnimate = response.data[i].images.fixed_height.url;
+                    animalImg.replaceWith(animalAnimate);
+
+                    console.log("animate");
+                });
+
+                }
+
+//trying to get gif to animate on click
+
+
+        })
+
+
 
 });
+
+
+
+
+/*if (state === "still") {
+        $(this).attr("src", results[i].images.fixed_height.url);
+        $(this).attr("data-state", "animate");
+    }*/
